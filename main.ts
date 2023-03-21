dbd94ee6a5b2251d2c06e0cff61a1513c3530125
RyanBoyette1 <127408681+RyanBoyette1@users.noreply.github.com>
2023-03-21 07:53:21 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,35 @@
 led.enable(false)
+pins.analogWritePin(AnalogPin.P2, 1023)
+pins.analogWritePin(AnalogPin.P3, 1023)
+pins.analogWritePin(AnalogPin.P4, 1023)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P6, 1)
-    basic.pause(10000)
-    pins.digitalWritePin(DigitalPin.P6, 0)
-    for (let index = 0; index < 4; index++) {
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P4, 1)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P4, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    basic.pause(1000)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+    pins.analogWritePin(AnalogPin.P3, 0)
+    basic.pause(1000)
+    pins.analogWritePin(AnalogPin.P4, 1023)
+    for (let index = 0; index < 1; index++) {
+        for (let index = 0; index <= 513; index++) {
+            pins.analogWritePin(AnalogPin.P2, index)
+            pins.analogWritePin(AnalogPin.P3, 1023 - index)
+            pins.analogWritePin(AnalogPin.P4, 512 - index)
+            basic.pause(1)
+        }
+        for (let index = 0; index <= 513; index++) {
+            pins.analogWritePin(AnalogPin.P2, 512 - index)
+            pins.analogWritePin(AnalogPin.P3, index)
+            pins.analogWritePin(AnalogPin.P4, 1023 - index)
+            basic.pause(1)
+        }
+        for (let index = 0; index <= 513; index++) {
+            pins.analogWritePin(AnalogPin.P2, 1023 - index)
+            pins.analogWritePin(AnalogPin.P3, 512 - index)
+            pins.analogWritePin(AnalogPin.P4, index)
+            basic.pause(1)
+        }
+        pins.analogWritePin(AnalogPin.P2, 1023)
+        pins.analogWritePin(AnalogPin.P3, 1023)
+        pins.analogWritePin(AnalogPin.P4, 1023)
     }
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P2, 0)
 })
